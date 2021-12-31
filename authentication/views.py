b90e1b8b1efd97e135fa16f29b4929c980309056
A-San96 <assanediatta96@gmail.com>
2021-12-31 13:01:45 +0000

    Use Class-Based View in auth app

--- authentication/views.py
+++ authentication/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.views.generic import View
 from . import forms
 
-def logout_user(request):
-    logout(request)
-    return redirect('login')
 
-def login_page(request):
-    form = forms.LoginForm()
-    message = ''
+class LoginPageView(View):
+    form_class = forms.LoginForm
+    template_name = 'authentication/login.html'
 
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             user = authenticate(
                 username = form.cleaned_data['username'],
@@ -19,8 +22,11 @@ def login_page(request):
             )
             if user is not None:
                 login(request, user)
-                message = f'Bonjour, {user.username}! Vous êtes connecté.'
-            else:
-                message = 'Identifiants Invalides.'
-    
-    return render(request, 'authentication/login.html', context={'form': form, 'message': message})
+                return redirect('home')
+        message = 'Identifiants Invalides.'
+        return render(request, self.template_name, context={'form': form, 'message': message})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def home(request):
-    return render(request, 'blog/home.html')
+
+class HomeView(LoginRequiredMixin, View):
+    template_name = 'blog/home.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
